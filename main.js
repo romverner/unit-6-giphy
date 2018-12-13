77e9b16dfe7f12f8a9bfdc76612136985ca0aa00
Roman Verner <romverj@gmail.com>
2018-12-12 21:46:49 -0500

    Initial files and some templates.

---
+++ main.js
@@ -0,0 +1,14 @@
+var topics = ["Castlevania Symphony of the Night", "Skyrim", "Mirror's Edge", "Call of Duty 4: Modern Warfare"];
+
+
+
+
+
+
+
+
+
+
+$(".gif-button").on("click", function() {
+
+});
